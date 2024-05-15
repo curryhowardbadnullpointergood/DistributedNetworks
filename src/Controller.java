2811eb927e3daacfc67ce34bd0c0c562c5348b3b
Ash <darkovl10@gmail.com>
2024-05-15 15:17:54 +0100

    made some notes

---
+++ src/Controller.java
@@ -0,0 +1,13 @@
+public class Controller {
+
+
+
+    // takes in a port, replication factor, a timeout, the last two I'm sure can be thought of as ints
+    // port is int as well I believe
+    public Controller (Integer cport, Integer replication_factor, Integer timeout_val){
+
+
+
+
+    }
+}
